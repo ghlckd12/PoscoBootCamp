25611b4c1a4895e0797d35674c0d1af01eed04b7
ghlckd12 <59240448+ghlckd12@users.noreply.github.com>
2024-04-25 15:16:03 +0900

    edit variable name

--- 20240425/20240425_prac1/20240425_prac1/prac1.cpp
+++ 20240425/20240425_prac1/20240425_prac1/prac1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void main()
 {
 	int arr[4][3] = { {19, 66, 68}, {20, 73, 94}, {19, 58, 37}, {20, 82, 29} };
-	int arrSize = sizeof(arr) / sizeof(arr[0]);
+	int arrRowSize = sizeof(arr) / sizeof(arr[0]);
 
 	string name[4] = { "방지턱", "정직한", "김가루", "조나단" };
 
@@ -13,7 +13,7 @@ void main()
 	int input;
 	cin >> input;
 
-	for (int i = 0; i < arrSize; i++)
+	for (int i = 0; i < arrRowSize; i++)
 	{
 		if (arr[i][0] == input)
 		{
