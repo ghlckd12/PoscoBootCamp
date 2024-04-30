b0342a81f622aff78e9f49eb9afa6aa34d050cdf
ghlckd12 <59240448+ghlckd12@users.noreply.github.com>
2024-04-30 20:30:44 +0900

    add prac4

---
+++ 20240430/20240430/prac4.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+void main()
+{
+
+	// 1. 사용자에게 몇 명의 학생을 입력할 것인지 묻기
+	int numStudent;
+	cout << "몇 명의 학생을 입력하나요? : ";
+	cin >> numStudent;
+
+	// 2. 학생 수 만큼 "이름, 나이, 생일" 순서로 한 번에 입력 받기
+	string** studentArr = new string* [numStudent];
+
+	for (int i = 0; i < numStudent; i++)
+	{
+		studentArr[i] = new string[3];
+	}
+
+	for (int i = 0; i < numStudent; i++) // 0 : 이름, 1 : 나이, 2 : 생일
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cin >> studentArr[i][j];
+		}
+	}
+
+	// 3. 학생 정보 출력
+	for (int i = 0; i < numStudent; i++)
+	{
+		cout << studentArr[i][0] << "의 나이는 " << studentArr[i][1] << "이고 생일은 " << studentArr[i][2] << "입니다." << endl;
+	}
+	
+	// 4. 평균 나이 출력
+	double resultSum = 0;
+	for (int i = 0; i < numStudent; i++)
+	{
+		resultSum += double(stoi(studentArr[i][1]));
+	}
+	
+	cout << "학생들의 평균 나이는 " << resultSum / double(numStudent) << "살 입니다." << endl;
+
+	// 5. 가장 빠른 생일 출력
+	int birthMin = INT32_MAX;
+	for (int i = 0; i < numStudent; i++)
+	{
+		if (stoi(studentArr[i][2]) < birthMin)
+		{
+			birthMin = stoi(studentArr[i][2]);
+		}
+	}
+	if (birthMin < 1000)
+	{
+		cout << "학생들 중 가장 빠른 생일은 0" << birthMin << " 입니다." << endl;
+	}
+	else
+	{
+		cout << "학생들 중 가장 빠른 생일은 " << birthMin << " 입니다." << endl;
+	}
+	// 6. 해제
+	for (int i = 0; i < 3; i++)
+		delete[] studentArr[i];
+
+	delete[] studentArr;
+}
